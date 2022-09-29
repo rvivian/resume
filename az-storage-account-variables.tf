853e87cdc830f0a3b3657d0053490b5bff97c319
Rhyss Vivian <rhyssv+github@gmail.com>
2022-09-29 13:41:30 -0800

    Setting default variables

--- az-storage-account-variables.tf
+++ az-storage-account-variables.tf
@@ -1,19 +1,24 @@
 variable "location" { #Location of the Azure resources
   type = string
+  default = "westus3"
 }
 
 variable "location_short_code" { #3-character short code for location
   type = string
+  default = "wu3"
 }
 
 variable "shortcode" { #business unit short code
   type = string
+  default = "rs"
 }
 
 variable "product" { #name of product or service being deployed
   type = string
+  default = "resume"
 }
 
 variable "envname" { #name of the environment being deployed to
   type = string
+  default = "dev"
 }

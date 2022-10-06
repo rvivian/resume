3ae613f49c0e655bb8bfd4340d38007edc0c3682
Rhyss Vivian <rhyssv+github@gmail.com>
2022-10-06 15:55:54 -0800

    All changes for now

--- cypress/e2e/resume-dev.cy.js
+++ cypress/e2e/resume-dev.cy.js
@@ -5,6 +5,11 @@ describe('Header', () => {
   it('has header information', () => {
     cy.title().should('include', 'Resume')
   })
+  it('has a left sidebar', () => {
+    cy.get('.left')
+      .should('have.class', 'left')
+      
+  })
   it('has a header', () => {
     cy.contains('Rhyss Vivian')
   })

58a0071fb4129ca5164414ca367baf948c7569a7
Rhyss Vivian <rhyssv+github@gmail.com>
2022-10-05 16:18:18 -0800

    Moving file copy

--- az-storage-account-output.tf
+++ az-storage-account-output.tf
@@ -9,3 +9,7 @@ output "azure_cdn_profile_name" {
 output "azure_cdn_endpoint" {
   value = azurerm_cdn_endpoint.cdn_endpoint.name
 }
+
+output "azure_storage_account" {
+  value = azurerm_storage_account.storage_account.name
+}

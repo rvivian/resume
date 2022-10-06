c7c3345a8dfe927ff6a26f413b92f28574c68965
Rhyss Vivian <rhyssv+github@gmail.com>
2022-10-06 15:01:24 -0800

    Removed temp cy, adding new test

--- cypress/e2e/resume-dev.cy.js
+++ cypress/e2e/resume-dev.cy.js
@@ -1,6 +1,9 @@
 describe('Header', () => {
   beforeEach(() => {
-    cy.visit('https://resume.northof66.com')
+    cy.visit('https://resume-dev.northof66.com')
+  })
+  it('has header information', () => {
+    cy.title().should('include', 'Resume')
   })
   it('has a header', () => {
     cy.contains('Rhyss Vivian')

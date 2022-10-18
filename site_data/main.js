cbf47ea316903cb0bf2ba54a6db52dfc7f52d1c8
Rhyss Vivian <rhyssv+github@gmail.com>
2022-10-18 14:05:50 -0800

    Fixed typo in Action

--- site_data/main.js
+++ site_data/main.js
@@ -7,7 +7,7 @@ const localApiUrl = 'http://localhost:7071/api/Counter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localApiUrl).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
